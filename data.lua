d0d183bff2a9a6cca752f22f1f247d9b79ec0360
Luizdodibre <128655111+HSp4m@users.noreply.github.com>
2024-10-04 10:42:01 -0400

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,58 @@
+return {
+  LOADER_version = "0.2.9",
+  LOADER_update_message = "This message will not be update anymore",
+  Games = {
+    ["0"] = {
+      name = "Arsenal",
+      id = "286090429",
+      version = "0.2.3",
+      update_message = "This message will not be update anymore (same for the version)"
+    },
+    ["1"] = {
+      name = "Legends of Speed",
+      id = "3101667897",
+      version = "0.1.1",
+      update_message = "This message will not be update anymore (same for the version)"
+    },
+    ["2"] = {
+      name = "Enforcement Fps",
+      id = "16564573060",
+      version = "0.2.4",
+      update_message = "This message will not be update anymore (same for the version)"
+    },
+    ["3"] = {
+      name = "Natural Disaster",
+      id = "189707",
+      version = "0.1",
+      update_message = "This message will not be update anymore (same for the version)"
+    },
+    ["4"] = {
+      name = "Ninja Legends",
+      id = "3956818381",
+      version = "0.1",
+      update_message = "This message will not be update anymore (same for the version)"
+    },
+    ["5"] = {
+      name = "Counter Blox",
+      id = "301549746",
+      version = "0.1",
+      update_message = "This message will not be update anymore (same for the version)"
+    },
+    ["6"] = {
+      name = "Counter Blox: Source 2",
+      id = "18966752111",
+      version = "0.1",
+      update_message = "This message will not be update anymore (same for the version)"
+    },
+    ["7"] = {
+      name = "Counter Blox: Reimagined V2",
+      id = "17586052747",
+      version = "0.1",
+      update_message = "This message will not be update anymore (same for the version)"
+    }
+  },
+  Universal = {
+    update_message = "This message will not be update anymore (same for the version)",
+    version = "0.2.2"
+  }
+}
