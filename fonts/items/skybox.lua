d1d801a609560e7c83627c9ecd5a36a6c2cd7248
Luizdodibre <128655111+HSp4m@users.noreply.github.com>
2025-07-11 00:26:57 -0400

    Create skybox.lua

---
+++ fonts/items/skybox.lua
@@ -0,0 +1,90 @@
+return {
+    ["Redshift"] = {
+        ["SkyboxBk"] = "rbxassetid://401664839",
+        ["SkyboxDn"] = "rbxassetid://401664862",
+        ["SkyboxFt"] = "rbxassetid://401664960",
+        ["SkyboxLf"] = "rbxassetid://401664881",
+        ["SkyboxRt"] = "rbxassetid://401664901",
+        ["SkyboxUp"] = "rbxassetid://401664936"
+    },
+    ["Night Sky"] = {
+        ["SkyboxBk"] = "rbxassetid://12064107",
+        ["SkyboxDn"] = "rbxassetid://12064152",
+        ["SkyboxFt"] = "rbxassetid://12064121",
+        ["SkyboxLf"] = "rbxassetid://12063984",
+        ["SkyboxRt"] = "rbxassetid://12064115",
+        ["SkyboxUp"] = "rbxassetid://12064131"
+    },
+    ["Purple Nebula"] = {
+        ["SkyboxBk"] = "rbxassetid://159454299",
+        ["SkyboxDn"] = "rbxassetid://159454296",
+        ["SkyboxFt"] = "rbxassetid://159454293",
+        ["SkyboxLf"] = "rbxassetid://159454286",
+        ["SkyboxRt"] = "rbxassetid://159454300",
+        ["SkyboxUp"] = "rbxassetid://159454288"
+    },
+    ["Morning Glow"] = {
+        ["SkyboxBk"] = "rbxassetid://1417494030",
+        ["SkyboxDn"] = "rbxassetid://1417494146",
+        ["SkyboxFt"] = "rbxassetid://1417494253",
+        ["SkyboxLf"] = "rbxassetid://1417494402",
+        ["SkyboxRt"] = "rbxassetid://1417494499",
+        ["SkyboxUp"] = "rbxassetid://1417494643"
+    },
+    ["Setting Sun"] = {
+        ["SkyboxBk"] = "rbxassetid://626460377",
+        ["SkyboxDn"] = "rbxassetid://626460216",
+        ["SkyboxFt"] = "rbxassetid://626460513",
+        ["SkyboxLf"] = "rbxassetid://626473032",
+        ["SkyboxRt"] = "rbxassetid://626458639",
+        ["SkyboxUp"] = "rbxassetid://626460625"
+    },
+    ["Cache"] = {
+        ["SkyboxBk"] = "rbxassetid://220513302",
+        ["SkyboxDn"] = "rbxassetid://213221473",
+        ["SkyboxFt"] = "rbxassetid://220513328",
+        ["SkyboxLf"] = "rbxassetid://220513318",
+        ["SkyboxRt"] = "rbxassetid://220513279",
+        ["SkyboxUp"] = "rbxassetid://220513345"
+    },
+    ["Fade Blue"] = {
+        ["SkyboxBk"] = "rbxassetid://153695414",
+        ["SkyboxDn"] = "rbxassetid://153695352",
+        ["SkyboxFt"] = "rbxassetid://153695452",
+        ["SkyboxLf"] = "rbxassetid://153695320",
+        ["SkyboxRt"] = "rbxassetid://153695383",
+        ["SkyboxUp"] = "rbxassetid://153695471"
+    },
+    ["Elegant Morning"] = {
+        ["SkyboxBk"] = "rbxassetid://153767241",
+        ["SkyboxDn"] = "rbxassetid://153767216",
+        ["SkyboxFt"] = "rbxassetid://153767266",
+        ["SkyboxLf"] = "rbxassetid://153767200",
+        ["SkyboxRt"] = "rbxassetid://153767231",
+        ["SkyboxUp"] = "rbxassetid://153767288"
+    },
+    ["Aesthetic Night"] = {
+        ["SkyboxBk"] = "rbxassetid://1045964490",
+        ["SkyboxDn"] = "rbxassetid://1045964368",
+        ["SkyboxFt"] = "rbxassetid://1045964655",
+        ["SkyboxLf"] = "rbxassetid://1045964655",
+        ["SkyboxRt"] = "rbxassetid://1045964655",
+        ["SkyboxUp"] = "rbxassetid://1045962969"
+    },
+    ["Neptune"] = {
+        ["SkyboxBk"] = "rbxassetid://218955819",
+        ["SkyboxDn"] = "rbxassetid://218953419",
+        ["SkyboxFt"] = "rbxassetid://218954524",
+        ["SkyboxLf"] = "rbxassetid://218958493",
+        ["SkyboxRt"] = "rbxassetid://218957134",
+        ["SkyboxUp"] = "rbxassetid://218950090"
+    },
+    ["Pink Daylight"] = {
+        ["SkyboxBk"] = "rbxassetid://271042516",
+        ["SkyboxDn"] = "rbxassetid://271077243",
+        ["SkyboxFt"] = "rbxassetid://271042556",
+        ["SkyboxLf"] = "rbxassetid://271042310",
+        ["SkyboxRt"] = "rbxassetid://271042467",
+        ["SkyboxUp"] = "rbxassetid://271077958"
+    }
+}
